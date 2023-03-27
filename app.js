246e333b8315fb953715274a4d2a3bcb10cf062a
magdaTrzTrash <mwt.trash@gmail.com>
2023-03-27 21:36:33 +0200

    function to save que, creating subque

--- app.js
+++ app.js
@@ -12,72 +12,51 @@ form.addEventListener("keydown", (e) => {
   if (e.keyCode == 13) {
     e.preventDefault();
     const queForm = e.target.closest("form");
-    let queTitle = "Zadanie 1";
-    let queSelect = "radio";
+    const queTitleTarget = queForm.querySelector("#question");
+    const queTitle = queTitleTarget.value;
 
-    if (e.target.tagName === "SELECT") {
-      queSelect = e.target.value;
-      const queTitleTarget = e.target.closest("input");
-      console.log(queTitleTarget);
-      //queTitle = queTitleTarget.value;
-      console.log("queSelect", queSelect);
-    } else if (e.target.tagName === "INPUT") {
-      queTitle = e.target.value;
-      console.log("queTitle", queTitle);
-      queSelectTarget = e.target.closest("#type");
-      queSelect = queSelectTarget.options[queSelectTarget.selectedIndex].value;
-    }
+    const queSelectTarget = queForm.querySelector("#type");
+    const queSelect = queSelectTarget.value;
 
-    console.log(e.target.value, e.target);
-    // Do something with the selected value
-    const newQuestion = {
-      id: queForm.getAttribute("id"),
-      question: queTitle,
-      type: queSelect,
-    };
-    console.log("keydown(): " + newQuestion);
-    localStorage.setItem("storage", JSON.stringify(newQuestion));
-    console.log("Enter 13code");
-    addQuestion();
-
+    addToStorage(queForm.getAttribute("id"), queTitle, queSelect);
     e.target.blur();
   }
 });
 
-// form.addEventListener("input", (e) => {});
-
+//Add next Question - button
 btnAccept.addEventListener("click", function (event) {
   event.preventDefault();
-  //   const divFragment = document.createElement("div");
+
   const formFragment = document.createElement("form");
   const id = Date.now();
   formFragment.setAttribute("id", id);
-  console.log("btnAccept() : " + id);
-
   form.appendChild(formFragment);
+
   addQueTypeElements(formFragment);
 });
 
-forms.forEach((form) => {
-  btnSubQue.onclick = function (event) {
-    event.preventDefault();
-    const questionTitle = inputQuestionTitle.value;
-    const questionType =
-      inputQuestionType.options[inputQuestionType.selectedIndex].value;
+function createSubQuestion(_form) {
+  console.log("createSubQuestion", _form);
+  const subQueTitleTarget = _form.querySelector("#question");
+  const subQueTitle = subQueTitleTarget.value;
 
-    console.log("addSubquestionBtn: " + questionType + questionTitle);
-    addFormElements(questionType);
-    addSubquestionBtn.remove();
-  };
-});
+  const subQueSelectTarget = _form.querySelector("#type");
+  const subQueSelect = subQueSelectTarget.value;
+  console.log("createSubQuestion: " + subQueSelect + subQueTitle);
 
-function addFormElements(answerType) {
+  const btnNewSubQue = _form.querySelector(".addSubquestion");
+  addFormElements(subQueSelect, _form);
+  btnNewSubQue.remove();
+}
+
+function addFormElements(_answerType, _form) {
   //   const divFragment = document.createElement("div");
   const formFragment = document.createElement("form");
-  console.log(answerType);
+  formFragment.classList.add("subQue");
+  console.log(_answerType);
   //   divFragment.setAttribute("class", "form");
 
-  if (answerType == "text" || answerType == "radio") {
+  if (_answerType == "text" || _answerType == "radio") {
     console.log("text || radio");
     const conditionLabel = document.createElement("label");
     conditionLabel.setAttribute("for", "condition");
@@ -85,17 +64,17 @@ function addFormElements(answerType) {
 
     formFragment.appendChild(conditionLabel);
 
-    if (answerType == "text") {
+    if (_answerType == "text") {
       const conditionInput = document.createElement("input");
       conditionInput.setAttribute("type", "text");
       conditionInput.setAttribute("id", "condition");
       formFragment.appendChild(conditionInput);
     }
-    if (answerType == "radio") {
+    if (_answerType == "radio") {
       console.log("radio");
-      const selectRadio = document.createAttribute("select");
-      select.setAttribute("id", "condition");
-      select.setAttribute("name", "typconditione");
+      const selectRadio = document.createElement("select");
+      selectRadio.setAttribute("id", "condition");
+      selectRadio.setAttribute("name", "typconditione");
 
       const yesOption = document.createElement("option");
       yesOption.setAttribute("value", "yes");
@@ -111,11 +90,11 @@ function addFormElements(answerType) {
     }
 
     // add the elements to the form
-    form.appendChild(formFragment);
-    addQueTypeElements(formFragment);
+    // form.insertAdjacentElement("afterend", formFragment);
+    // addQueTypeElements(formFragment);
   }
 
-  if (answerType == "number") {
+  if (_answerType == "number") {
     console.log("number");
     const conditionLabel = document.createElement("label");
     conditionLabel.setAttribute("for", "condition");
@@ -125,13 +104,16 @@ function addFormElements(answerType) {
     conditionInput.setAttribute("type", "text");
     conditionInput.setAttribute("id", "condition");
 
-    // add the elements to the form
-    form.appendChild(formFragment);
+    // // add the elements to the form
+    // form.appendChild(formFragment);
     formFragment.appendChild(conditionLabel);
     formFragment.appendChild(conditionInput);
 
-    addQueTypeElements(formFragment);
+    // addQueTypeElements(formFragment);
   }
+  // add the elements to the form
+  _form.insertAdjacentElement("afterend", formFragment);
+  addQueTypeElements(formFragment);
 }
 
 function addQueTypeElements(_formFragment) {
@@ -168,11 +150,11 @@ function addQueTypeElements(_formFragment) {
   // create the elements for the buttons
   const addSubquestionBtn = document.createElement("button");
   addSubquestionBtn.setAttribute("class", "addSubquestion");
+  addSubquestionBtn.addEventListener("click", function (event) {
+    const form = _formFragment;
+    createSubQuestion(form);
+  });
   addSubquestionBtn.textContent = "Add Sub-question";
-
-  // const nextQuestionBtn = document.createElement("button");
-  // nextQuestionBtn.setAttribute("id", "nextQuestion");
-  // nextQuestionBtn.textContent = "Add next Question";
 
   // add the elements to the form
   select.appendChild(radioOption);
@@ -184,4 +166,17 @@ function addQueTypeElements(_formFragment) {
   _formFragment.appendChild(selectLabel);
   _formFragment.appendChild(select);
   _formFragment.appendChild(addSubquestionBtn);
+}
+
+function addToStorage(_id, _question, _type) {
+  const question = {
+    id: _id,
+    question: _question,
+    type: _type,
+  };
+  console.log("addToStorage(): " + question);
+
+  let questions = JSON.parse(localStorage.getItem("questions")) || [];
+  questions.push(question);
+  localStorage.setItem("questions", JSON.stringify(questions));
 }
